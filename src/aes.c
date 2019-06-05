b77fb01bc762ba70663d1dd829fcd9e930995084
Daniel Stotts <daniel.stotts@labs.cset.oit.edu>
2019-06-05 11:55:24 -0700

    Initial commit

---
+++ src/aes.c
@@ -0,0 +1,13 @@
+#include "aes.h"
+
+char * encrypt(const char * message)
+{
+	*message;
+	return 0;
+}
+
+char * decrypt(const char * cipher)
+{
+	*cipher;
+	return 0;
+}
---
+++ src/aes.h
@@ -0,0 +1,7 @@
+#ifndef AES_IMPL_H
+#define AES_IMPL_H
+
+extern char * encrypt(const char * message);
+extern char * decrypt(const char * cipher);
+
+#endif
