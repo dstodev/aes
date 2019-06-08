f2b621b95ffa6815478f9ee9d65a2af536898113
Daniel Stotts <dstodev@gmail.com>
2019-06-08 16:19:48 -0700

    Cleaned up main.c

--- src/main.c
+++ src/main.c
@@ -4,20 +4,15 @@
 
 #include "aes.h"
 
-int main(int argc, char * argv[])
+int main()
 {
+	// AES NIST example key
 	const char * key = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
 
 	aes_cipher_t cipher;
 	char * message = 0;
 
-	// for (int i = 1; i < argc; ++i) {
-	// 	printf("%2d. %s\n", i, argv[i]);
-	// }
-
-	// cipher = encrypt("this is a test, lmao", "\x0f\x47\x0c\xaf\x15\xd9\xb7\x7f\x71\xe8\xad\x67\xc9\x59\xd6\x98");
-	// cipher = encrypt("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "0123456789ABCDEF");
-
+	// Encrypt AES NIST example message
 	cipher = encrypt("\x32\x43\xf6\xa8\x88\x5a\x30\x8d\x31\x31\x98\xa2\xe0\x37\x07\x34", key);
 
 	if (cipher.cipherLen) {
@@ -30,7 +25,6 @@ int main(int argc, char * argv[])
 		if (message) {
 			free(message);
 		}
-
 		free(cipher.cipher);
 	}
 
